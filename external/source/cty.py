621617189f3a3f1fe7df186d059cc2b8bc228bc6
エルジー🐧 <elzyproot@protonmail.com>
2026-07-02 10:32:37 +0700

    Refactor AES decryption and library loading functions
    Refactor AES decryption functions and add error handling for library loading.

--- external/source/cty.py
+++ external/source/cty.py
@@ -1,14 +1,43 @@
 import ctypes
 import smf
-from apps.utility.colors import CC
+from apps.utility.colors import *
 
 
-def libc():
+def libcrp():
     try:
-        return ctypes.CDLL("libcrypto.so.3")
+        crypto = ctypes.CDLL("libcrypto.so.3")
+        
+        class AES_KEY(ctypes.Structure):
+            _fields_ = [("rd_key", ctypes.c_uint * 60), ("rounds", ctypes.c_int)]
+
+        crypto.AES_set_decrypt_key.argtypes = [
+            ctypes.c_char_p,
+            ctypes.c_int,
+            ctypes.POINTER(AES_KEY),
+        ]
+        crypto.AES_decrypt.argtypes = [ctypes.c_char_p, ctypes.c_char_p, ctypes.POINTER(AES_KEY)]
+
+        return crypto
     except OSError as e:
         smf.printf(
             f"[*] {CC.YELLOW}This module is not supported on this platform.{CC.RESET}"
         )
         smf.printd("Failed to load libcrypto.so.3", e, level="INFO")
         return None
+
+def libc():
+    try:
+        return ctypes.CDLL("libc.so.6", use_errno=True)
+    except OSError as e:
+        smf.printf(f"{CC.YELLOW}This module is not supported on this platform.{CC.RESET}")
+        smf.printd("Failed to load libc.so.6", e, level="INFO")
+        return None
+
+def aes_decrypt_block(block):
+    key = AES_KEY()
+    crypto.AES_set_decrypt_key(KEY, 128, ctypes.byref(key))
+    out = ctypes.create_string_buffer(16)
+    crypto.AES_decrypt(block, out, ctypes.byref(key))
+    smf.printd(f"Output decript: {block}", out.raw[:16], level="INFO")
+    return out.raw[:16]
+    
