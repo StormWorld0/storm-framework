2ebf6965642f32e45216bb5f38abfb5791c168ae
エルジー🐧 <elzyproot@protonmail.com>
2026-07-02 12:21:32 +0700

    Enhance log extraction with traceback and formatting
    Updated log extraction function to include traceback and improved logging output formatting with color codes.

--- lib/smfdb_helpers/log_utils.py
+++ lib/smfdb_helpers/log_utils.py
@@ -4,7 +4,7 @@ import rootmap
 import smf
 
 from pathlib import Path
-
+from apps.utility.colors import *
 
 def extract_logs(level_target: str, output_file: str = "log.txt"):
     """
@@ -29,7 +29,7 @@ def extract_logs(level_target: str, output_file: str = "log.txt"):
 
         # Advanced SQL Queries (Take from the most recent)
         query = """
-            SELECT timestamp, level, label, payload, caller_info 
+            SELECT timestamp, level, label, payload, traceback, caller_info 
             FROM system_logs 
             WHERE level = ? 
             ORDER BY timestamp DESC
@@ -40,7 +40,7 @@ def extract_logs(level_target: str, output_file: str = "log.txt"):
 
         # Validation if data is empty
         if not rows:
-            smf.printf("[!] No log level =>", level_query)
+            smf.printf(f"[!]{CC.YELLOW} No log level =>{CC.RESET}", level_query)
             return
 
         home_dir = Path.home()
@@ -62,7 +62,7 @@ def extract_logs(level_target: str, output_file: str = "log.txt"):
 
             # Data Iteration
             for row in rows:
-                ts, lvl, label, payload, caller = row
+                ts, lvl, label, payload, traceback, caller = row
 
                 # Convert f64 Unix Epoch to common Date Format
                 dt_str = datetime.datetime.fromtimestamp(ts).strftime(
@@ -70,22 +70,29 @@ def extract_logs(level_target: str, output_file: str = "log.txt"):
                 )[:-3]
 
                 # Writing format like server log
-                f.write(f"[{dt_str}] [{lvl}] <{caller}>\n")
+                f.write(f"[{dt_str}] [{lvl}]\n")
+                f.write(f" CALLER  : {caller}\n")
                 f.write(f" LABEL   : {label}\n")
 
                 # If the payload contains any content, display it. Otherwise, ignore it.
                 if payload:
                     f.write(f" PAYLOAD : {payload}\n")
 
+                if traceback_data:
+                    f.write(" TRACEBACK:\n")
+                    # Indent to make it look neat in text files
+                    for line in traceback.split('\n'):
+                        f.write(f"     {line}\n")
+
                 f.write("-" * 60 + "\n")
 
         smf.printd("System log extracted by user", final_output_path, level="INFO")
-        smf.printf(f"[✓] Extraction Successful! {len(rows)} log lines =>", level_query)
-        smf.printf("[!] File saved to =>", final_output_path)
+        smf.printf(f"[✓]{CC.GREEN} Extraction Successful! {len(rows)} log lines =>{CC.RESET}", level_query)
+        smf.printf(f"[!]{CC.YELLOW} File saved to =>{CC.RESET}", final_output_path)
 
     except sqlite3.Error as e:
-        smf.printf(f"[-] A Database Log I/O error occurred")
+        smf.printf(f"[-]{CC.RED} A Database Log I/O error occurred{CC.RESET}")
         smf.printd("A Database Log I/O error occurred", e, level="ERROR")
     except Exception as e:
-        smf.printf(f"[-] Extraction failure occurred")
+        smf.printf(f"[-]{CC.RED} Extraction failure occurred{CC.RESET}")
         smf.printd("Extraction failure occurred", e, level="ERROR")
