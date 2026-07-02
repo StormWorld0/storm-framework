f110f21c8edf5c3cd80d1d3f8b184e4ea4ab0458
エルジー🐧 <elzyproot@protonmail.com>
2026-07-02 10:06:54 +0700

    Refactor ctyp function to libc and improve error handling

--- external/source/cty.py
+++ external/source/cty.py
@@ -1,12 +1,11 @@
+import ctypes
 import smf
-from apps.utility.colors import *
+from apps.utility.colors import CC
 
-def ctyp()
+def libc():
     try:
-        import ctypes
-        crypto = ctypes.CDLL("libcrypto.so.3")
-        return crypto
-    except Exception as e:
-        smf.printf(f"[*] {CC.YELLOW}Android environment does not support this module{CC.RESET}")
-        smf.printd("Error calling libcrypto to Linux", e, level="INFO")
+        return ctypes.CDLL("libcrypto.so.3")
+    except OSError as e:
+        smf.printf(f"[*] {CC.YELLOW}This module is not supported on this platform.{CC.RESET}")
+        smf.printd("Failed to load libcrypto.so.3", e, level="INFO")
         return None
