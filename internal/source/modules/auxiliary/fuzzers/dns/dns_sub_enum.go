9621f4d5c621b79cff9d34baeb3f95ebee2bdb3d
エルジー🐧 <elzyproot@protonmail.com>
2026-07-13 06:15:15 +0700

    Update dns_sub_enum.go

--- internal/source/modules/auxiliary/fuzzers/dns/dns_sub_enum.go
+++ internal/source/modules/auxiliary/fuzzers/dns/dns_sub_enum.go
@@ -91,7 +91,7 @@ func worker(jobs <-chan Job, wg *sync.WaitGroup, foundCounter *int32, processedC
 			}
 
 			if resp.StatusCode < 400 || resp.StatusCode == 403 || resp.StatusCode == 401 {
-				fmt.Printf("FOUND => %d | %-40s | %-25s | %s\n", resp.StatusCode, j.URL, server, contentType)
+				fmt.Printf("FOUND => %d | %-40s | %-10s | %s\n", resp.StatusCode, j.URL, server, contentType)
 				atomic.AddInt32(foundCounter, 1)
 			}
 		}(job)
