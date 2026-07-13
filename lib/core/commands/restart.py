cfe9637878d5528bb338f8dcf5b2d396f3342be8
エルジー🐧 <elzyproot@protonmail.com>
2026-07-13 20:24:53 +0700

    Create restart.py

---
+++ lib/core/commands/restart.py
@@ -0,0 +1,16 @@
+# -- https://github.com/StormWorld0/storm-framework
+# -- SMF License
+import smf
+
+from apps.utility.colors import *
+from apps.utility.restart import run_restart as restart
+
+# This is to restart and save the variables that were set before restarting and then restore them.
+# This is good if we experience a bug or error failure when we are ready to execute.
+# by storing old variable data, it is very profitable and speeds up the time
+def execute(args, ctx):
+    options = ctx.options
+    try:
+        restart(options)
+    except Exception as e:
+        smf.printd("Restarting failed", e, level="ERROR")
