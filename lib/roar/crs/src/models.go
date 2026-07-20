9db6697a05a31e66700f2775975af1053422cd95
エルジー🐧 <elzyproot@protonmail.com>
2026-07-21 02:01:01 +0700

    Create models.go

---
+++ lib/roar/crs/src/models.go
@@ -0,0 +1,25 @@
+// internal/source/models.go
+package main
+
+// RequestPacket adalah cerminan dari dictionary yang dikirim Python
+type RequestPacket struct {
+	Primitive string            `json:"primitive"`
+	
+	// Parameter HTTP
+	Method    string            `json:"method,omitempty"`
+	URL       string            `json:"url,omitempty"`
+	Body      string            `json:"body,omitempty"`
+	Headers   map[string]string `json:"headers,omitempty"`
+	
+	// Parameter Umum
+	Timeout   float64           `json:"timeout,omitempty"`
+}
+
+// ResponsePacket adalah balasan yang akan dibaca oleh Python
+type ResponsePacket struct {
+	Status  string      `json:"status"`
+	Message string      `json:"message,omitempty"`
+	
+	// Data bebas (bisa map, array, string)
+	Data    interface{} `json:"data,omitempty"`
+}
