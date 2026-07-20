1f3b2e5f1ad191603b45b2cd8a775a86297833c2
エルジー🐧 <elzyproot@protonmail.com>
2026-07-21 01:37:04 +0700

    Create dns.py

---
+++ lib/roar/crs/protocol/dns.py
@@ -0,0 +1,20 @@
+# -- https://github.com/StormWorld0/storm-framework
+# -- License SMF
+# -- Author zxelzy
+import smf
+
+from apps.utility.colors import *
+from ..transport import CRS
+
+def dns_request(domain: str, record_type: str = "A", timeout: float = 2.0, **kwargs) -> dict:
+    packet = {
+        "primitive": "DNS_LOOKUP",
+        "domain": domain,
+        "record_type": record_type,
+        "timeout": timeout
+    }
+    if kwargs:
+        smf.printf(f"[!] {CC.YELLOW} Data is not recognized =>{CC.RESET}", kwargs)
+        pass
+        
+    return CRS.send(packet)
