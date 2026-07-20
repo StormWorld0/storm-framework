8cc2804257da5c5b8c8f8495b653cd54f946e34f
エルジー🐧 <elzyproot@protonmail.com>
2026-07-21 01:45:22 +0700

    Create http.py

---
+++ lib/roar/crs/protocol/http.py
@@ -0,0 +1,31 @@
+# -- https://github.com/StormWorld0/storm-framework
+# -- License SMF
+# -- Author zxelzy
+import smf
+
+from apps.utility.colors import CC
+from ..transport import CRS
+
+def http_request(
+    method: str, 
+    url: str, 
+    headers: dict = None, 
+    body: str = "", 
+    timeout: float = 5.0, 
+    **kwargs
+) -> dict:
+    """Wrapper to send HTTP Request to CRS Engine"""
+    
+    packet = {
+        "primitive": "HTTP_SEND",
+        "method": method.upper(),
+        "url": url,
+        "headers": headers or {},
+        "body": body,
+        "timeout": timeout
+    }
+    
+    if kwargs:
+        smf.printf(f"[!] {CC.YELLOW}Unrecognized parameters dropped =>{CC.RESET}", kwargs)
+        
+    return CRS.send(packet)
