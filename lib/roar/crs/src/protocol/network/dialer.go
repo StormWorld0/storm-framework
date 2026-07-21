c8580101a5f54c97374988317ec55dffaa7b4298
エルジー🐧 <elzyproot@protonmail.com>
2026-07-22 01:07:37 +0700

    Create dialer.go

---
+++ lib/roar/crs/src/protocol/network/dialer.go
@@ -0,0 +1,56 @@
+// https://github.com/StormWorld0/storm-framework
+// License SMF
+// Author zxelzy
+package network
+
+import (
+	"sync"
+	"time"
+
+	"github.com/projectdiscovery/fastdialer/fastdialer"
+)
+
+var (
+	// globalDialer menyimpan instance Fastdialer untuk digunakan seluruh modul
+	globalDialer *fastdialer.Dialer
+	once         sync.Once
+	initErr      error
+)
+
+// InitGlobalDialer dipanggil satu kali saat framework baru menyala (bootstrap)
+func InitGlobalDialer() error {
+	once.Do(func() {
+		options := fastdialer.DefaultOptions
+		options.DialerTimeout = 5 * time.Second
+		options.DialerKeepAlive = 5 * time.Second
+		options.WithDialerHistory = true      // Melacak history IP untuk debugging
+		options.EnableFallback = true         // DNS fallback otomatis
+		
+		// Inisialisasi engine fastdialer (DNS Cache, Connection Pooler)
+		dialer, err := fastdialer.NewDialer(options)
+		if err != nil {
+			initErr = err
+			return
+		}
+		globalDialer = dialer
+	})
+	return initErr
+}
+
+// GetDialer mengembalikan instance Fastdialer yang sudah berjalan.
+// Modul Socket/HTTP HANYA boleh menggunakan fungsi ini, dilarang membuat Dialer baru.
+func GetDialer() *fastdialer.Dialer {
+	// Jika belum diinisialisasi (misal lupa dipanggil di main.go), kita inisialisasi darurat
+	if globalDialer == nil {
+		_ = InitGlobalDialer()
+	}
+	return globalDialer
+}
+
+// Close membersihkan DNS cache dan menutup koneksi yang menggantung saat framework dimatikan
+func Close() {
+	if globalDialer != nil {
+		globalDialer.Close()
+	}
+}
+
