6918d68e14059cc059690e164bb229ccc247c048
エルジー🐧 <elzyproot@protonmail.com>
2026-07-21 11:34:40 +0700

    Update packet.go

--- lib/roar/crs/src/packet/packet.go
+++ lib/roar/crs/src/packet/packet.go
@@ -4,11 +4,16 @@ package packet
 type RequestPacket struct {
 	Primitive string            `json:"primitive"`
 	
-	// Parameter HTTP
+	// HTTP
 	Method    string            `json:"method,omitempty"`
 	URL       string            `json:"url,omitempty"`
 	Body      string            `json:"body,omitempty"`
 	Headers   map[string]string `json:"headers,omitempty"`
+
+	// DNS
+    Domain   string             `json:"domain,omitempty"`
+    Type     string             `json:"type,omitempty"`      // A, AAAA, MX, TXT, ...
+    Protocol string             `json:"protocol,omitempty"`  // udp, tcp
 	
 	// Parameter umum
 	Timeout   float64           `json:"timeout,omitempty"`
