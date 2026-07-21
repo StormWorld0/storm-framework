0da4eef7a668126661559dea92cb144c162cb2ea
エルジー🐧 <elzyproot@protonmail.com>
2026-07-21 12:45:32 +0700

    Update registry.go

--- lib/roar/crs/src/protocol/registry.go
+++ lib/roar/crs/src/protocol/registry.go
@@ -2,6 +2,7 @@ package protocol
 
 import (
 	h "github.com/StormWorld0/storm-framework/lib/roar/crs/src/protocol/http"
+	d "github.com/StormWorld0/storm-framework/lib/roar/crs/src/protocol/http"
 	"github.com/StormWorld0/storm-framework/lib/roar/crs/src/packet"
 )
 
@@ -10,6 +11,6 @@ type Handler func(packet.RequestPacket) packet.ResponsePacket
 // Handlers Registry (Map)
 var Handlers = map[string]Handler{
 	"HTTP_SEND": h.HTTP,
-	// "DNS_LOOKUP": DNS,
+	"DNS_SEND": d.DNS,
 	// "TCP_CONNECT": TCP,
 }
