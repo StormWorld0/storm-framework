4f5d1f8b72c6b1d7ed34e30f7a34ad1e6a38aafd
エルジー🐧 <elzyproot@protonmail.com>
2026-07-22 20:24:17 +0700

    Update web_head.py

--- modules/auxiliary/scanner/net/web_head.py
+++ modules/auxiliary/scanner/net/web_head.py
@@ -36,7 +36,7 @@ def execute(options, net):
             "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36"
         }
         response = net.http_request(
-            "get", url, headers=headers, timeout=5, verify=False, redirect=False
+            "get", url, headers=headers, timeout=5, verify=True, redirect=False
         )
         for header, value in response["data"]["headers"].items():
             smf.printf(f"  {C.HEADER}{header}:{C.RESET} {value}")
