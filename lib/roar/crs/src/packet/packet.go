790c7c8d858385fcf95510fb63d3bba19c785003
エルジー🐧 <elzyproot@protonmail.com>
2026-07-23 05:05:09 +0700

    Update packet.go

--- lib/roar/crs/src/packet/packet.go
+++ lib/roar/crs/src/packet/packet.go
@@ -6,18 +6,18 @@ type RequestPacket struct {
 	
 	// HTTP
 	Method    string            `json:"method,omitempty"`    // GET, POST, PUT, DELETE, ...
-	URL       string            `json:"url,omitempty"`
-	Body      string            `json:"body,omitempty"`
-	Headers   map[string]string `json:"headers,omitempty"`
+	URL       string            `json:"url,omitempty"`       // https / http
+	Body      string            `json:"body,omitempty"`      // The body can be anything including the payload
+	Headers   map[string]string `json:"headers,omitempty"`   // Header can be customized if socket
 
 	// DNS
-    Domain   string             `json:"domain,omitempty"`
+    Domain   string             `json:"domain,omitempty"`    // example.com
     Type     string             `json:"type,omitempty"`      // A, AAAA, MX, TXT, ...
     Protocol string             `json:"protocol,omitempty"`  // udp, tcp, tls, ssl
 
 	// Standard parameters
-	Host      string            `json:"host,omitempty"`      // URL / IP
-	Ip        int               `json:"ip,omitempty"`
+	Host      string            `json:"host,omitempty"`      // URL / IP / Domain
+	Ip        int               `json:"ip,omitempty"`        // 127.0.0.1
 	Port      int               `json:"port,omitempty"`      // PORT 1 - 65535
 	
 	// General parameters
@@ -28,15 +28,13 @@ type RequestPacket struct {
 	InfoTLS   bool              `json:"info_tls"`            // True / False
 	RateLimit int               `json:"ratelimit"`           // 0 = Unlimited
 	Retry     int               `json:"retry"`               // Retry connection
-	Encoding  string            `json:"encoding,omitempty"`  // Hex / Text
+	Encoding  string            `json:"encoding,omitempty"`  // Hex
 	ReadSize  int               `json:"readsize"`            // Limit Read Buffer
 }
 
 // ResponsePacket
 type ResponsePacket struct {
-	Status     string      `json:"status"`
-	Message    string      `json:"message,omitempty"`
-	
-	// Free data (can be map, array, string)
-	Data       interface{} `json:"data,omitempty"`
+	Status     string      `json:"status"`             // ERROR / SUCCESS / TIMEOUT / ...
+	Message    string      `json:"message,omitempty"`  // Error message or success message
+	Data       interface{} `json:"data,omitempty"`     // Free data (can be map, array, string)
 }
