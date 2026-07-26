1dd5bf17e0bb903c216bb77f1350528a6e5b574a
エルジー🐧 <elzyproot@protonmail.com>
2026-07-26 23:28:10 +0700

    Update config.go

--- lib/roar/crs/src/utils/config.go
+++ lib/roar/crs/src/utils/config.go
@@ -6,3 +6,14 @@ import(
 
 // SessionManager thread-safe
 var ActiveSessions = sync.Map{} // map[string]net.Conn
+
+// Struct untuk menyimpan koneksi + state metadata TLS-nya
+type SessionState struct {
+	Conn       net.Conn
+	TLSCert    string
+	TLSKey     string
+	TLSCA      string
+	Verify     bool
+	IsTLS      bool
+	Host       string
+}
