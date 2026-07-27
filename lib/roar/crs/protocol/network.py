e726a4a4a6fbdfbd5e92747b264429f61e2d13f0
エルジー🐧 <elzyproot@protonmail.com>
2026-07-27 20:30:28 +0700

    Update network.py

--- lib/roar/crs/protocol/network.py
+++ lib/roar/crs/protocol/network.py
@@ -21,7 +21,7 @@ class Socket:
         host: str = "",
         port: int = 0,
         protocol: str = "tcp",
-        timeout: float = 10.0,
+        timeout: float = 30.0,
         readsize: int = 0,
         ratelimit: int = 0,
         sessid: str = "",
