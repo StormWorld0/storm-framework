63fc37eeba22eee24fe8614f0dd36588f5e4c216
エルジー🐧 <elzyproot@protonmail.com>
2026-07-27 14:15:14 +0700

    Update config.go

--- lib/roar/crs/src/utils/config.go
+++ lib/roar/crs/src/utils/config.go
@@ -18,3 +18,25 @@ type SessionState struct {
 	IsTLS      bool
 	Host       string
 }
+
+func (s *SessionState) Read(b []byte) (n int, err error)   { return s.Conn.Read(b) }
+func (s *SessionState) Write(b []byte) (n int, err error)  { return s.Conn.Write(b) }
+func (s *SessionState) Close() error                       { return s.Conn.Close() }
+func (s *SessionState) LocalAddr() net.Addr                { return s.Conn.LocalAddr() }
+func (s *SessionState) RemoteAddr() net.Addr               { return s.Conn.RemoteAddr() }
+func (s *SessionState) SetDeadline(t time.Time) error      { return s.Conn.SetDeadline(t) }
+func (s *SessionState) SetReadDeadline(t time.Time) error { return s.Conn.SetReadDeadline(t) }
+func (s *SessionState) SetWriteDeadline(t time.Time) error{ return s.Conn.SetWriteDeadline(t) }
+
+// Interface Helper untuk TLS Connection State
+type tlsStateProvider interface {
+	ConnectionState() tls.ConnectionState
+}
+
+// Memungkinkan type assertion ConnectionState() tembus ke socket TLS di dalamnya
+func (s *SessionState) ConnectionState() tls.ConnectionState {
+	if getter, ok := s.Conn.(tlsStateProvider); ok {
+		return getter.ConnectionState()
+	}
+	return tls.ConnectionState{}
+}
