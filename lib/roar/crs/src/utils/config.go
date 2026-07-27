c95754bb548495c54c347c73b5dd840a6fe3dfbd
エルジー🐧 <elzyproot@protonmail.com>
2026-07-27 14:40:19 +0700

    Update config.go

--- lib/roar/crs/src/utils/config.go
+++ lib/roar/crs/src/utils/config.go
@@ -1,44 +1,21 @@
 package utils
 
 import (
-	"crypto/tls"
 	"net"
 	"sync"
-	"time"
 )
 
 // SessionManager thread-safe
 var ActiveSessions = sync.Map{} // map[string]net.Conn
 
+var SessionTLSMap = sync.Map{} // map[string]TLSMetadata
+
 // Struct untuk menyimpan koneksi + state metadata TLS-nya
-type SessionState struct {
-	Conn       net.Conn
+type TLSMetadata struct {
 	TLSCert    string
 	TLSKey     string
 	TLSCA      string
 	Verify     bool
 	IsTLS      bool
 	Host       string
-}
-
-func (s *SessionState) Read(b []byte) (n int, err error)   { return s.Conn.Read(b) }
-func (s *SessionState) Write(b []byte) (n int, err error)  { return s.Conn.Write(b) }
-func (s *SessionState) Close() error                       { return s.Conn.Close() }
-func (s *SessionState) LocalAddr() net.Addr                { return s.Conn.LocalAddr() }
-func (s *SessionState) RemoteAddr() net.Addr               { return s.Conn.RemoteAddr() }
-func (s *SessionState) SetDeadline(t time.Time) error      { return s.Conn.SetDeadline(t) }
-func (s *SessionState) SetReadDeadline(t time.Time) error { return s.Conn.SetReadDeadline(t) }
-func (s *SessionState) SetWriteDeadline(t time.Time) error{ return s.Conn.SetWriteDeadline(t) }
-
-// Interface Helper untuk TLS Connection State
-type tlsStateProvider interface {
-	ConnectionState() tls.ConnectionState
-}
-
-// Memungkinkan type assertion ConnectionState() tembus ke socket TLS di dalamnya
-func (s *SessionState) ConnectionState() tls.ConnectionState {
-	if getter, ok := s.Conn.(tlsStateProvider); ok {
-		return getter.ConnectionState()
-	}
-	return tls.ConnectionState{}
 }
