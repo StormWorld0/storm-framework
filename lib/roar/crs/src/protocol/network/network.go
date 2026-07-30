696ce4a683f76e5384b95785bb977dbafd421d90
エルジー🐧 <elzyproot@protonmail.com>
2026-07-30 07:36:56 +0700

    Update network.go

--- lib/roar/crs/src/protocol/network/network.go
+++ lib/roar/crs/src/protocol/network/network.go
@@ -382,28 +382,30 @@ func Network(req packet.RequestPacket) packet.ResponsePacket {
     }
 	
 	var tlsData map[string]interface{}
-	if stateGetter, ok := conn.(tlsConnStateGetter); ok {
-		state := stateGetter.ConnectionState()
-		if state.HandshakeComplete || state.Version != 0 {
-			tlsData = map[string]interface{}{
-				"tls_version":    tlsVersionString(state.Version),
-				"cipher_suite":   tls.CipherSuiteName(state.CipherSuite),
-				"protocol":       state.NegotiatedProtocol,
-				"hostname":       state.ServerName,
-				"handshake":      state.HandshakeComplete,
-				"session_resume": state.DidResume,
-			}
-			if len(state.PeerCertificates) > 0 {
-				cert := state.PeerCertificates[0]
-				tlsData["subject"] = cert.Subject.CommonName
-				tlsData["issuer"] = cert.Issuer.CommonName
-				tlsData["dns_name"] = cert.DNSNames
-				tlsData["expires"] = cert.NotAfter.Format(time.RFC3339)
-			}
-			if len(state.VerifiedChains) > 0 {
-				tlsData["cert_chain_count"] = len(state.VerifiedChains)
-			}
-		}
+	if req.InfoTLS {
+	    if stateGetter, ok := conn.(tlsConnStateGetter); ok {
+	    	state := stateGetter.ConnectionState()
+		    if state.HandshakeComplete || state.Version != 0 {
+		    	tlsData = map[string]interface{}{
+			    	"tls_version":    tlsVersionString(state.Version),
+			    	"cipher_suite":   tls.CipherSuiteName(state.CipherSuite),
+			    	"protocol":       state.NegotiatedProtocol,
+			    	"hostname":       state.ServerName,
+			    	"handshake":      state.HandshakeComplete,
+			    	"session_resume": state.DidResume,
+		    	}
+		    	if len(state.PeerCertificates) > 0 {
+		    		cert := state.PeerCertificates[0]
+			    	tlsData["subject"] = cert.Subject.CommonName
+		    		tlsData["issuer"] = cert.Issuer.CommonName
+		    		tlsData["dns_name"] = cert.DNSNames
+		    		tlsData["expires"] = cert.NotAfter.Format(time.RFC3339)
+		    	}
+		    	if len(state.VerifiedChains) > 0 {
+		    		tlsData["cert_chain_count"] = len(state.VerifiedChains)
+     	    	}
+	    	}
+	    }
 	}
 
 	
