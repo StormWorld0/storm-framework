67010af960c605c62ca7a9871d6b79d40a7d876e
エルジー🐧 <elzyproot@protonmail.com>
2026-07-31 20:03:37 +0700

    Update test_socket.py

--- modules/tested/test_socket.py
+++ modules/tested/test_socket.py
@@ -37,7 +37,7 @@ def execute(options, net):
 
         # 3. Test Upgrade TLS (Memanggil method uptls)
         smf.printf("\n[2] Upgrading Socket Session to TLS...")
-        up = sock.uptls(cert="", key="", verify=False, timeout=10)
+        up = sock.uptls(cert="", key="", verify=False)
 
         res = up.get("status")
         if res == "SUCCESS":
