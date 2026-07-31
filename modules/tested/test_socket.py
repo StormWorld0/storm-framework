00770cf9955356b2941e2d8d60bedde6d4e9c60c
エルジー🐧 <elzyproot@protonmail.com>
2026-07-31 20:01:18 +0700

    Update test_socket.py

--- modules/tested/test_socket.py
+++ modules/tested/test_socket.py
@@ -23,26 +23,35 @@ def execute(options, net):
 
     smf.printf(f"[+] Initializing IPC Session to {ip}:{port}...")
 
-    sock = net.Socket(host=ip, port=port, mode="duplex", infotls=True)
+    sock = net.Socket(host=ip, port=port, mode="duplex")
 
     try:
         # 2. Test TCP Send / Plaintext First
         smf.printf("\n[1] Sending Plaintext via TCP...")
-        res_send = sock.send(data="PING_PLAINTEXT\n", mode="duplex")
-        raw_send = res_send.get("data").get("raw_bytes")
-        smf.printf(f"    Raw Response: {raw_send}")
+        res_send = sock.send(data="PING_PLAINTEXT\n", mode="duplex", timeout=10)
+
+        rep = res_send.get("status")
+        if rep == "SUCCESS":
+            raw_send = res_send.get("data").get("raw_bytes")
+            smf.printf(f"    Raw Response: {raw_send}")
 
         # 3. Test Upgrade TLS (Memanggil method uptls)
         smf.printf("\n[2] Upgrading Socket Session to TLS...")
-        up = sock.uptls(cert="", key="", verify=False)
-        info = up.get("data").get("info_tls").get("tls_version")
-        smf.printf(f"    TLS Info     : {info}")
+        up = sock.uptls(cert="", key="", verify=False, timeout=10)
+
+        res = up.get("status")
+        if res == "SUCCESS":
+            info = up.get("data").get("info_tls").get("tls_version")
+            smf.printf(f"    TLS Info     : {info}")
 
         # 4. Test Send Data Terenkripsi di atas Session TLS yang Sama (Reused)
         smf.printf("\n[3] Sending Encrypted Data over Reused Session...")
-        res_tls = sock.send(data="HELLO_TLS_ENCRYPTED\n", mode="duplex")
-        raw = res_tls.get("data").get("raw_bytes")
-        smf.printf(f"    Raw Response: {raw}")
+        res_tls = sock.send(data="HELLO_TLS_ENCRYPTED\n", mode="duplex", timeout=10)
+        
+        st = res_tls.get("status")
+        if st == "SUCCESS":
+            raw = res_tls.get("data").get("raw_bytes")
+            smf.printf(f"    Raw Response: {raw}")
 
     except Exception as e:
         smf.printf(f"\n[-] Error saat execution: {e}")
