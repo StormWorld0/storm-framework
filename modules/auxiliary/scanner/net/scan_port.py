26c573ae7f7d654895e3b6b1d74438695ac55dce
エルジー🐧 <elzyproot@protonmail.com>
2026-08-01 15:32:47 +0700

    Fix response status check in scan_port.py

--- modules/auxiliary/scanner/net/scan_port.py
+++ modules/auxiliary/scanner/net/scan_port.py
@@ -45,7 +45,7 @@ def get_service_banner(target_ip, port, net):
         res = s.recv(1024)
 
         # Jika Go berhasil melakukan Dial (Socket Terbuka)
-        if res.issuccess:
+        if res.status == "SUCCESS":
             status_color = f"{C.SUCCESS} OPEN " + STATUS_OPEN
             banner_info = "No version information."
 
