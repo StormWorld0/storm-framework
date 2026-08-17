84a4783fe877ffad2e86486541aa36503f7afcd7
エルジー🐧 <elzyproot@protonmail.com>
2026-08-18 03:42:00 +0700

    Rename reap_zombies to reap_zombie and update logic

--- lib/pid_manager.py
+++ lib/pid_manager.py
@@ -1,4 +1,5 @@
 import os
+import smf
 import signal
 from typing import Set
 
@@ -15,22 +16,22 @@ class PIDManager:
             cls._tracked_pids.add(pid)
 
     @classmethod
-    def reap_zombies(cls) -> None:
+    def reap_zombie(cls) -> None:
         """Non-blocking check pada seluruh PID terdaftar"""
         dead_pids = set()
 
         for pid in list(cls._tracked_pids):
             try:
                 # os.WNOHANG: Non-blocking call
-                # pid_reaped > 0 artinya proses sudah mati & berhasil di-reap dari OS
+                # pid_reaped > 0 artinya proses sudah mati
                 pid_reaped, _ = os.waitpid(pid, os.WNOHANG)
                 if pid_reaped != 0:
                     dead_pids.add(pid)
             except ChildProcessError:
-                # PID sudah bukan child process aktif / sudah di-reap oleh OS
+                # PID sudah bukan child process aktif
                 dead_pids.add(pid)
-            except Exception:
-                pass
+            except Exception as e:
+                smf.printd("Failed to reap zombie process throwing exception", e, level="WARN")
 
         cls._tracked_pids -= dead_pids
 
@@ -43,20 +44,21 @@ class PIDManager:
                 os.kill(pid, signal.SIGTERM)
                 # Reaping langsung dari Kernel
                 os.waitpid(pid, 0)
-            except (ProcessLookupError, ChildProcessError):
-                pass
-            except Exception:
-                # Jika SIGTERM gagal, paksa dengan SIGKILL
+            except (ProcessLookupError, ChildProcessError) as e:
+                smf.printd("ProcessLookupError, ChildProcessError", e, level="WARN")
+            except Exception as e:
+                smf.printd("Failed to execute SIGTERM process. Fallback SIGKILL", e, level="WARN")
+                # Fallback SIGKILL
                 try:
                     os.kill(pid, signal.SIGKILL)
                     os.waitpid(pid, 0)
-                except Exception:
-                    pass
+                except Exception as e:
+                    smf.printd("Failed to SIGKILL process", e, level="WARN")
 
         cls._tracked_pids.clear()
 
     @classmethod
     def prepare(cls, new_pid: int) -> None:
         """Mencatat PID proses baru & Mematikan PID proses zombie"""
-        cls.reap_zombies()
+        cls.reap_zombie()
         cls.register(new_pid)
