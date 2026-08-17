dd25c0c6171ccb1590823ffc93d90e7db0c01993
エルジー🐧 <elzyproot@protonmail.com>
2026-08-18 06:14:46 +0700

    Refactor banner function to use smf.printf

--- lib/ui/banner.py
+++ lib/ui/banner.py
@@ -1,19 +1,28 @@
+# --- https://github.com/StormWorld0/storm-framework ---
+# SMF License
+# copyright (c) 2026
+# Complete information about the License is in the root directory.
+# Author: zxelzy
+
 import os
 import smf
 import apps.base.config_ui as ui
 
-from apps.base.config_update import *
+from apps.base.config_update import check_update
 from apps.banners.uib import banner_live
 
-
+# To clean the terminal before loading anything so it is clean
+# load banners to the main interface randomly if more than one banner is available
+# Loads main module data statistics for information on the number of available modules
+# Check for the latest updates, if any, it will display the latest update information.
 def banner():
     try:
         os.system("clear")
-        print(banner_live())
+        smf.printf(banner_live())
         ui.stormUI()
         check_update()
-    except ImportError as d:
-        smf.printd("IMPORT BANNER ERROR", d, level="ERROR")
+    except ImportError as e:
+        smf.printd("IMPORT BANNER ERROR", e, level="ERROR")
         return
     except Exception as e:
         smf.printd("ERROR BANNER EXCEPTION", e, level="ERROR")
