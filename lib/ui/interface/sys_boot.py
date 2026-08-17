597acc36442f89103a6fb92c7fd6816db188b6cd
エルジー🐧 <elzyproot@protonmail.com>
2026-08-18 04:08:44 +0700

    Add system booting functionality in sys_boot.py

---
+++ lib/ui/interface/sys_boot.py
@@ -0,0 +1,15 @@
+import readline  # noqa: F401
+
+try:
+    from lib.smf.core.booting.boot import *
+    from ..banner import *
+    from .start_interfc import *
+except ImportError as e:
+    smf.printf(f"Error import =>", e, file=sys.stderr)
+    sys.exit(100)
+
+
+def system_booting():
+    boot()
+    banner()
+    main()
