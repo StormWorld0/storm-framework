5ed4a154bfbd22cb2dd516607c9482fad3daadb7
エルジー🐧 <elzyproot@protonmail.com>
2026-08-18 06:48:08 +0700

    Revise license and author comments in engine.py
    Updated license information and author details in engine.py.

--- lib/smf/core/console/engine.py
+++ lib/smf/core/console/engine.py
@@ -1,5 +1,9 @@
-# -- https://github.com/StormWorld0/storm-framework
-# -- SMF License
+# --- https://github.com/StormWorld0/storm-framework ---
+# SMF License
+# copyright (c) 2026
+# Complete information about the License is in the root directory.
+# Author: zxelzy
+
 import typing
 import smf
 import os
