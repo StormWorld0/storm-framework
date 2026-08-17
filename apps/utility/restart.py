906922729ea273dd541e7daebac94e7dcab2bd89
エルジー🐧 <elzyproot@protonmail.com>
2026-08-18 03:51:15 +0700

    Improve error message in restart function
    Updated error message for clarity during restart failure.

--- apps/utility/restart.py
+++ apps/utility/restart.py
@@ -1,18 +1,26 @@
+# -- https://github.com/StormWorld0/storm-framework
+# -- License SMF
+# -- Author: zxelzy
+
 import os
 import sys
 import smf
 import lib.smf.core.sf.svch as svch
 
+from lib.pid_manager import PIDManager as pid
+
 
 def run_restart(options):
     # save old variables
     svch.session(options)
+    # Kill the active process
+    pid.cleanup()
     # Restart the storm
     executable = sys.argv[0]
     args = sys.argv
     try:
         os.execv(executable, args)
     except Exception as e:
-        smf.printd("ERROR RESTART", e, level="ERROR")
+        smf.printd("Error while restarting", e, level="ERROR")
         smf.printf(f"[!] Restart failed")
         sys.exit(1)
