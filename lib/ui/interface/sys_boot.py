bf057f83816ec42a1d5b4f9f3b1949d993e792dc
エルジー🐧 <elzyproot@protonmail.com>
2026-08-18 06:27:10 +0700

    Refactor system booting initialization and imports
    Refactor imports and update booting process to use aliases for better clarity.

--- lib/ui/interface/sys_boot.py
+++ lib/ui/interface/sys_boot.py
@@ -1,16 +1,25 @@
+# --- https://github.com/StormWorld0/storm-framework ---
+# SMF License
+# copyright (c) 2026
+# Complete information about the License is in the root directory.
+# Author: zxelzy
+
 import readline  # noqa: F401
 import sys
 
 try:
-    from lib.smf.core.booting.boot import *
-    from ..banner import *
-    from .start_interfc import *
+    from lib.smf.core.booting.boot import boot as sysb
+    from ..banner import banner as style_ui
+    from .start_interfc import main
 except ImportError as e:
     smf.printf(f"Error import interface =>", e, file=sys.stderr)
     sys.exit(100)
 
-
+# Perform step by step initialization starting before entering the interface
+# Booting to run the initial steps and determine security and stability
+# Loading banner for style framework
+# call main to activate (Read Eval Print Loop) interface
 def system_booting():
-    boot()
-    banner()
+    sysb()
+    style_ui()
     main()
