2fd8ae938bb529baec56c88ed120c9333b9fa0ab
エルジー🐧 <elzyproot@protonmail.com>
2026-08-24 14:04:32 +0700

    Fix typo in failure message formatting

--- modules/auxiliary/brute/telnet/telnet_brute.py
+++ modules/auxiliary/brute/telnet/telnet_brute.py
@@ -87,7 +87,7 @@ def execute(options, net):
                 if r < 0:
                     # Username ditolak, tidak perlu lanjut ke password
                     smf.printf(
-                        f"{CC.YELLOW}[*] Username: {username} {SYS_FAILED}{CC.RESET}"
+                        f"{CC.YELLOW}[*] Username: {username} {SYM_FAILED}{CC.RESET}"
                     )
                     break  # keluar dari loop password, lanjut ke username berikutnya
 
@@ -95,7 +95,7 @@ def execute(options, net):
                 _, r = con.send(password, expected=promt_shell)
                 if r < 0:
                     smf.printf(
-                        f"{CC.YELLOW}[*] U:{username} P:{password} {SYS_FAILED}{CC.RESET}"
+                        f"{CC.YELLOW}[*] U:{username} P:{password} {SYM_FAILED}{CC.RESET}"
                     )
 
                 if r >= 0:
