689aa5ad5eb69a5c2ca52cb37461ce6430749db9
エルジー🐧 <elzyproot@protonmail.com>
2026-08-31 20:32:52 +0700

    Implement PID cleanup in exit command
    Added PID cleanup before exiting to prevent crashes.

--- lib/core/commands/exit.py
+++ lib/core/commands/exit.py
@@ -1,10 +1,13 @@
 # -- https://github.com/StormWorld0/storm-framework
 # -- SMF License
 
+from lib.pid_manager import PIDManager as pid
 
 # Exit command to avoid errors or crashes in storm.
 # Because if you only use CTRL + C it is possible that the storm will come out messy.
 # This will minimize the possibility of a crash to prevent damage.
 def execute(args, ctx):
+    # Kill all running process PIDs
+    pid.cleanup()
     # Mutate state exit in-place
     ctx.exit = True
