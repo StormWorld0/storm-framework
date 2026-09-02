760f4e8b6274f373c5d3b4f9aac26f6fccd789ff
エルジー🐧 <elzyproot@protonmail.com>
2026-09-02 21:03:09 +0700

    Add ExtractTLSInfoFromState function for TLS data

--- lib/roar/crs/src/tls/tls-config.go
+++ lib/roar/crs/src/tls/tls-config.go
@@ -34,30 +34,34 @@ func tlsVersionString(v uint16) string {
 	}
 }
 
+func ExtractTLSInfoFromState(state tls.ConnectionState) map[string]interface{} {
+	tlsData := map[string]interface{}{
+		"tls_version":    tlsVersionString(state.Version),
+		"cipher_suite":   tls.CipherSuiteName(state.CipherSuite),
+		"protocol":       state.NegotiatedProtocol,
+		"hostname":       state.ServerName,
+		"handshake":      state.HandshakeComplete,
+		"session_resume": state.DidResume,
+	}
+	if len(state.PeerCertificates) > 0 {
+		cert := state.PeerCertificates[0]
+		tlsData["subject"] = cert.Subject.CommonName
+		tlsData["issuer"] = cert.Issuer.CommonName
+		tlsData["dns_name"] = cert.DNSNames
+		tlsData["expires"] = cert.NotAfter.Format(time.RFC3339)
+	}
+	if len(state.VerifiedChains) > 0 {
+		tlsData["cert_chain"] = len(state.VerifiedChains)
+	}
+	return tlsData
+}
+
 // ExtractTLSInfo menarik state kriptografi dari koneksi.
 func ExtractTLSInfo(conn net.Conn) map[string]interface{} {
 	if stateGetter, ok := conn.(tlsConnStateGetter); ok {
 		state := stateGetter.ConnectionState()
 		if state.HandshakeComplete || state.Version != 0 {
-			tlsData := map[string]interface{}{
-				"tls_version":    tlsVersionString(state.Version),
-				"cipher_suite":   tls.CipherSuiteName(state.CipherSuite),
-				"protocol":       state.NegotiatedProtocol,
-				"hostname":       state.ServerName,
-				"handshake":      state.HandshakeComplete,
-				"session_resume": state.DidResume,
-			}
-			if len(state.PeerCertificates) > 0 {
-				cert := state.PeerCertificates[0]
-				tlsData["subject"] = cert.Subject.CommonName
-				tlsData["issuer"] = cert.Issuer.CommonName
-				tlsData["dns_name"] = cert.DNSNames
-				tlsData["expires"] = cert.NotAfter.Format(time.RFC3339)
-			}
-			if len(state.VerifiedChains) > 0 {
-				tlsData["cert_chain"] = len(state.VerifiedChains)
-			}
-			return tlsData
+			return ExtractTLSInfoFromState(state)
 		}
 	}
 	return nil
