f01e94b68d6819665b974d5161d1c50877fa9551
エルジー🐧 <elzyproot@protonmail.com>
2026-09-03 10:22:02 +0700

    Remove unused import 'discovery' from __init__.py

--- lib/roar/crs/protocol/__init__.py
+++ lib/roar/crs/protocol/__init__.py
@@ -1,4 +1,4 @@
-from .dns import requests, discovery  # noqa
+from .dns import requests # noqa
 from .http import http_requests  # noqa
 from .network import Socket  # noqa
 from .telnet import TelnetClient  # noqa
