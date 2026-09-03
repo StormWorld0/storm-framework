79f70748a57055619fab06c16eaa3652b4fb547e
エルジー🐧 <elzyproot@protonmail.com>
2026-09-03 23:53:47 +0700

    Add WHOIS handler to the registry

--- lib/roar/crs/src/protocol/registry.go
+++ lib/roar/crs/src/protocol/registry.go
@@ -3,6 +3,7 @@ package protocol
 import (
 	h "github.com/StormWorld0/storm-framework/lib/roar/crs/src/protocol/http"
 	d "github.com/StormWorld0/storm-framework/lib/roar/crs/src/protocol/dns"
+	w "github.com/StormWorld0/storm-framework/lib/roar/crs/src/protocol/whois"
 	n "github.com/StormWorld0/storm-framework/lib/roar/crs/src/protocol/network"
 	"github.com/StormWorld0/storm-framework/lib/roar/crs/src/packet"
 )
@@ -13,5 +14,6 @@ type Handler func(packet.RequestPacket) packet.ResponsePacket
 var Handlers = map[string]Handler{
 	"HTTP_SEND": h.HTTP,
 	"DNS_SEND": d.DNS,
+	"WHOIS_SEND": w.WHOIS
 	"NETWORK_SEND": n.Network,
 }
