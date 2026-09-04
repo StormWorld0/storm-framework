90355fe6c8ac8e40be22936ec8d7132379c982e8
エルジー🐧 <elzyproot@protonmail.com>
2026-09-04 13:18:23 +0700

    Import domwhois in addition to ipwhois

--- lib/roar/crs/protocol/__init__.py
+++ lib/roar/crs/protocol/__init__.py
@@ -2,4 +2,4 @@ from .dns import requests  # noqa
 from .http import http_requests  # noqa
 from .network import Socket  # noqa
 from .telnet import TelnetClient  # noqa
-from .whois import ipwhois  # noqa
+from .whois import ipwhois, domwhois  # noqa
