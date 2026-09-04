9a56cbc734a9bdd71889eb32fb91eea1973c4077
エルジー🐧 <elzyproot@protonmail.com>
2026-09-04 12:14:08 +0700

    Update contact print formatting in whois_ip.py

--- modules/auxiliary/scanner/net/whois_ip.py
+++ modules/auxiliary/scanner/net/whois_ip.py
@@ -25,31 +25,31 @@ def execute(options, net):
         r = net.IPWhois(target_ip, timeout=5.0, con=20)
         if r.ok:
             # TECHNICAL
-            smf.printf(f"{CC.CYAN}[TECHNICAL CONTACT]{CC.RESET}")
+            smf.printf(f"[✓] {CC.CYAN}[TECHNICAL CONTACT]{CC.RESET}")
             for contact in r.technical:
                 for k, v in contact.items():
-                    smf.printf(f"{CC.GREEN}{k:<12}{CC.RESET} = {CC.YELLOW}{v}{CC.RESET}")
+                    smf.printf(f"{CC.GREEN}    {k:<12}{CC.RESET} = {CC.YELLOW}{v}{CC.RESET}")
                 smf.printf()
 
             # ADMIN
-            smf.printf(f"{CC.CYAN}[ADMINISTRATIVE CONTACT]{CC.RESET}")
+            smf.printf(f"[✓] {CC.CYAN}[ADMINISTRATIVE CONTACT]{CC.RESET}")
             for contact in r.admin:
                 for k, v in contact.items():
-                    smf.printf(f"{CC.GREEN}{k:<12}{CC.RESET} = {CC.YELLOW}{v}{CC.RESET}")
+                    smf.printf(f"{CC.GREEN}    {k:<12}{CC.RESET} = {CC.YELLOW}{v}{CC.RESET}")
                 smf.printf()
 
             # ABUSE
-            smf.printf(f"{CC.CYAN}[ABUSE CONTACT]{CC.RESET}")
+            smf.printf(f"[✓] {CC.CYAN}[ABUSE CONTACT]{CC.RESET}")
             for contact in r.abuse:
                 for k, v in contact.items():
-                    smf.printf(f"{CC.GREEN}{k:<12}{CC.RESET} = {CC.YELLOW}{v}{CC.RESET}")
+                    smf.printf(f"{CC.GREEN}    {k:<12}{CC.RESET} = {CC.YELLOW}{v}{CC.RESET}")
                 smf.printf()
 
             # REGISTRANT
-            smf.printf(f"{CC.CYAN}[REGISTRANT CONTACT]{CC.RESET}")
+            smf.printf(f"[✓] {CC.CYAN}[REGISTRANT CONTACT]{CC.RESET}")
             for contact in r.registrant:
                 for k, v in contact.items():
-                    smf.printf(f"{CC.GREEN}{k:<12}{CC.RESET} = {CC.YELLOW}{v}{CC.RESET}")
+                    smf.printf(f"{CC.GREEN}    {k:<12}{CC.RESET} = {CC.YELLOW}{v}{CC.RESET}")
                 smf.printf()
     except KeyboardInterrupt:
         return
