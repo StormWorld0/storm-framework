54c0b460f88068f704a22bc4e1298192056e1319
エルジー🐧 <elzyproot@protonmail.com>
2026-09-04 13:22:11 +0700

    Add DWhois to imports and __all__ list

--- lib/roar/crs/net_api.py
+++ lib/roar/crs/net_api.py
@@ -2,6 +2,7 @@ from .protocol import (
     http_requests as HTTPR,
     requests as DNSL,
     ipwhois as IPWhois,
+    domwhois as DWhois,
     Socket,
     TelnetClient as Telnet,
 )
@@ -12,6 +13,7 @@ __all__ = [
     "HTTPR",
     "DNSL",
     "IPWhois",
+    "DWhois",
     "Socket",
     "Telnet",
 ]
