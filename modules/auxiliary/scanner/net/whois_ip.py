a9bdabac2fe3ed8be4c6db1c55097a6c9c5644c7
エルジー🐧 <elzyproot@protonmail.com>
2026-09-05 10:23:15 +0700

    Refactor contact display logic in whois_ip.py

--- modules/auxiliary/scanner/net/whois_ip.py
+++ modules/auxiliary/scanner/net/whois_ip.py
@@ -24,41 +24,45 @@ def execute(options, net):
     try:
         r = net.IPWhois(target_ip, timeout=5.0, con=20)
         if r.ok:
-            # TECHNICAL
-            smf.printf(f"\n[✓] {CC.CYAN}[TECHNICAL CONTACT]{CC.RESET}")
-            for contact in r.technical:
-                for k, v in contact.items():
-                    smf.printf(
-                        f"{CC.GREEN}    {k:<12}{CC.RESET} = {CC.YELLOW}{v}{CC.RESET}"
-                    )
-                smf.printf()
+            if r.technical and r._categorized_contacts.get("Technical"):
+                # TECHNICAL
+                smf.printf(f"\n[✓] {CC.CYAN}[TECHNICAL CONTACT]{CC.RESET}")
+                for contact in r.technical:
+                    for k, v in contact.items():
+                        smf.printf(
+                            f"{CC.GREEN}    {k:<12}{CC.RESET} = {CC.YELLOW}{v}{CC.RESET}"
+                        )
+                    smf.printf()
 
-            # ADMIN
-            smf.printf(f"[✓] {CC.CYAN}[ADMINISTRATIVE CONTACT]{CC.RESET}")
-            for contact in r.admin:
-                for k, v in contact.items():
-                    smf.printf(
-                        f"{CC.GREEN}    {k:<12}{CC.RESET} = {CC.YELLOW}{v}{CC.RESET}"
-                    )
-                smf.printf()
+            if r.admin and r._categorized_contacts.get("Administrative"):
+                # ADMIN
+                smf.printf(f"[✓] {CC.CYAN}[ADMINISTRATIVE CONTACT]{CC.RESET}")
+                for contact in r.admin:
+                    for k, v in contact.items():
+                        smf.printf(
+                            f"{CC.GREEN}    {k:<12}{CC.RESET} = {CC.YELLOW}{v}{CC.RESET}"
+                        )
+                    smf.printf()
 
-            # ABUSE
-            smf.printf(f"[✓] {CC.CYAN}[ABUSE CONTACT]{CC.RESET}")
-            for contact in r.abuse:
-                for k, v in contact.items():
-                    smf.printf(
-                        f"{CC.GREEN}    {k:<12}{CC.RESET} = {CC.YELLOW}{v}{CC.RESET}"
-                    )
-                smf.printf()
+            if r.abuse and r._categorized_contacts.get("Abuse"):
+                # ABUSE
+                smf.printf(f"[✓] {CC.CYAN}[ABUSE CONTACT]{CC.RESET}")
+                for contact in r.abuse:
+                    for k, v in contact.items():
+                        smf.printf(
+                            f"{CC.GREEN}    {k:<12}{CC.RESET} = {CC.YELLOW}{v}{CC.RESET}"
+                        )
+                    smf.printf()
 
-            # REGISTRANT
-            smf.printf(f"[✓] {CC.CYAN}[REGISTRANT CONTACT]{CC.RESET}")
-            for contact in r.registrant:
-                for k, v in contact.items():
-                    smf.printf(
-                        f"{CC.GREEN}    {k:<12}{CC.RESET} = {CC.YELLOW}{v}{CC.RESET}"
-                    )
-                smf.printf()
+            if r.registrant and r._categorized_contacts.get("Registrant"):
+                # REGISTRANT
+                smf.printf(f"[✓] {CC.CYAN}[REGISTRANT CONTACT]{CC.RESET}")
+                for contact in r.registrant:
+                    for k, v in contact.items():
+                        smf.printf(
+                            f"{CC.GREEN}    {k:<12}{CC.RESET} = {CC.YELLOW}{v}{CC.RESET}"
+                        )
+                    smf.printf()
         else:
             smf.printf(f"[!] {CC.CYAN}Invalid WhoisIP:{CC.RESET}")
             smf.printf(f"   {CC.YELLOW}   Status      => {r.status}")
