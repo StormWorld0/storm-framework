e4ca2efd5570de0ae7749307ee3e08afc7d1891b
エルジー🐧 <elzyproot@protonmail.com>
2026-09-09 15:46:02 +0700

    Add __all__ to __init__.py for exports

--- config/__init__.py
+++ config/__init__.py
@@ -8,3 +8,14 @@ from .config import (
     DB_PORT,  # noqa
     LOG_FORMAT,  # noqa
 )
+
+__all__ = [
+    "SMF_BASE_DIR",
+    "DB_DATA_DIR",
+    "CONFIG_FILE",
+    "DB_USER",
+    "DB_NAME",
+    "DB_HOST",
+    "DB_PORT",
+    "LOG_FORMAT",
+]
