7ae5f8102d7b85f903ef77bf1501ccead9cd8373
エルジー🐧 <elzyproot@protonmail.com>
2026-09-09 12:44:52 +0700

    Refactor PluginResult class to be generic

--- lib/roar/plugin/result/resp_plugin.py
+++ lib/roar/plugin/result/resp_plugin.py
@@ -1,17 +1,24 @@
 from dataclasses import dataclass
-from typing import Any, Optional
+from typing import Any, Optional, Generic, TypeVar
 
+T = TypeVar('T')
 
 @dataclass
-class PluginResult:
+class PluginResult(Generic[T]):
+    """Standardized response container for all plugin executions."""
+    
     success: bool  # Must be True or False
     data: Optional[Any] = None  # Place of results if SUCCESSFUL
     error: Optional[str] = None  # Place to order if FAIL
 
     @classmethod
-    def ok(cls, data: Any = None):
+    def ok(cls, data: Any = None) -> "PluginResult":
         return cls(success=True, data=data)
 
     @classmethod
-    def fail(cls, msg: str):
-        return cls(success=False, error=msg)
+    def fail(cls, msg: any) -> "PluginResult":
+        return cls(success=False, error=str(msg))
+
+    @classmethod
+    def __bool__(cls) -> bool:
+        return cls.ok
