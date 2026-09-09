451038eec6e0f927b04598622210b7db79dd8f5d
エルジー🐧 <elzyproot@protonmail.com>
2026-09-09 17:54:31 +0700

    Refactor CA certificate generation script

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -38,29 +38,17 @@ if [ ! -f .env ]; then
     write_to_file "STORM_API=https://api.cant.workers.dev" ".env" true
 fi
 
-if [ ! -f data/smf_ca.key ] || [ ! -f data/smf_ca.crt ]; then
-    cd "$APP_ROOT/data"
+if [ ! -f config/smf_ca.key ] || [ ! -f config/smf_ca.crt ]; then
+    cd "$APP_ROOT/config"
     
     openssl genrsa -out smf_ca.key 2048 >/dev/null 2>&1
     chmod 600 smf_ca.key
 
-    cat > openssl_v3.cnf <<-EOF
-[req]
-distinguished_name = req_distinguished_name
-[req_distinguished_name]
-[v3_ca]
-subjectKeyIdentifier = hash
-authorityKeyIdentifier = keyid:always,issuer
-basicConstraints = critical, CA:true
-keyUsage = critical, digitalSignature, cRLSign, keyCertSign
-EOF
-
     # Redirect stderr ke /dev/null untuk mematikan log subject & issuer
     openssl req -x509 -new -nodes -key smf_ca.key -sha256 -days 3650 -out smf_ca.crt \
         -subj "/CN=Storm Trusted Root CA/O=StormWorld0/OU=Network-Security-Storm" \
-        -extensions v3_ca -config openssl_v3.cnf >/dev/null 2>&1
+        -extensions v3_ca -config openssl-ca.conf >/dev/null 2>&1
 
-    rm openssl_v3.cnf
     cd "$APP_ROOT"
 fi
 
