51ad3b4a665e727f4f3f20c3f299db830dfed249
エルジー🐧 <elzyproot@protonmail.com>
2026-09-10 23:29:14 +0700

    Enhance domain_to_ip to support IPv4 and IPv6
    Refactor domain_to_ip function to return separate lists for IPv4 and IPv6 addresses.

--- apps/utility/parse.py
+++ apps/utility/parse.py
@@ -1,8 +1,9 @@
 from urllib.parse import urlparse
 import socket
-
+import ipaddress
 
 def parse_url(url):
+    """Returns a Dict of URL fragments"""
     val = url.strip()
     if "://" not in val:
         res = "//" + val
@@ -20,9 +21,34 @@ def parse_url(url):
 
 
 def domain_to_ip(domain):
+    """Returning IPV4 and IPV6"""
     try:
-        results = socket.getaddrinfo(domain, None, proto=socket.IPPROTO_TCP)
-        ips = sorted({result[4][0] for result in results})
-        return ips
+        results = socket.getaddrinfo(
+            domain,
+            None,
+            proto=socket.IPPROTO_TCP
+        )
+
+        ipv4 = set()
+        ipv6 = set()
+
+        for result in results:
+            ip = result[4][0]
+            try:
+                addr = ipaddress.ip_address(ip)
+                if addr.version == 4:
+                    ipv4.add(ip)
+                elif addr.version == 6:
+                    ipv6.add(ip)
+            except ValueError:
+                continue
+
+        return {
+            "ipv4": sorted(ipv4),
+            "ipv6": sorted(ipv6),
+        }
     except socket.gaierror:
-        return []
+        return {
+            "ipv4": [],
+            "ipv6": [],
+        }
