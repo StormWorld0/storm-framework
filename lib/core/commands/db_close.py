e6a05cc16d1768c08ea175f81398965d1393f23b
エルジー🐧 <elzyproot@protonmail.com>
2026-09-12 16:46:19 +0700

    Refactor db_close.py to improve clarity and comments
    Added comments to explain the purpose of closing the database connection and modified the try-except structure for clarity.

--- lib/core/commands/db_close.py
+++ lib/core/commands/db_close.py
@@ -3,13 +3,16 @@ import smf
 from apps.utility.colors import CC
 from lib.smf.postgresql import close_db
 
+# Closes the database connection to PostgreSQL that is currently active in the background
+# for security in database processes that are running in the background process
+# and also to avoid exception errors from external libraries and internal libraries
+# to cause data corruption in PostgreSQL.
 def execute(args, ctx):
-    try:
-        resp = close_db()
+    resp = close_db()
 
-        if resp is None:
-            smf.printf(f"[!]{CC.YELLOW} There are no active database connections!{CC.RESET}")
-        elif resp is False:
-            smf.printf(f"[!]{CC.RED} Failed to close database connection!{CC.RESET}")
-        else:
-            smf.printf(f"[✓]{CC.GREEN} Successfully closed the database connection{CC.RESET}")
+    if resp is None:
+        smf.printf(f"[!]{CC.YELLOW} There are no active database connections!{CC.RESET}")
+    elif resp is False:
+        smf.printf(f"[!]{CC.RED} Failed to close database connection!{CC.RESET}")
+    else:
+        smf.printf(f"[✓]{CC.GREEN} Successfully closed the database connection{CC.RESET}")
