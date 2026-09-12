d22e8e980a77ea6c71c9b165e72be805e59c8354
エルジー🐧 <elzyproot@protonmail.com>
2026-09-12 17:04:35 +0700

    Improve database connection status messages
    Updated the db_status.py script to improve the output messages for database connection status. Adjusted default values for database name, backend, and current workspace.

--- lib/core/commands/db_status.py
+++ lib/core/commands/db_status.py
@@ -3,7 +3,8 @@ import smf
 from apps.utility.colors import CC
 from lib.smf.postgresql import get_status
 
-
+# Check the status of the database connection to PostgreSQL, to find out whether the framework is connected or not
+# and all message statuses, workspaces, and database names will be displayed.
 def execute(args, ctx):
     db = ctx.db
     if not db:
@@ -12,17 +13,18 @@ def execute(args, ctx):
 
     status_data = get_status()
     sts = status_data.get("status")
+    rsn = status_data.get("reason")
     if sts == "connected":
-        db_name = status_data.get("database", "smf")
-        backend = status_data.get("backend", "PostgreSQL")
-        current_ws = getattr(db, "current_workspace", "default")
+        db_name = status_data.get("database", "")
+        backend = status_data.get("backend", "")
+        current_ws = getattr(db, "current_workspace", "")
 
         smf.printf(
-            f"[*]{CC.YELLOW} Connected to ({db_name}). Connection type:{CC.GREEN} {backend}. {CC.YELLOW}Workspace:{CC.GREEN} {current_ws}{CC.RESET}"
+            f"[*]{CC.YELLOW} Connected to ({CC.GREEN}{db_name}{CC.YELLOW}). Connection type:{CC.GREEN} {backend}. {CC.YELLOW}Workspace:{CC.GREEN} {current_ws}{CC.RESET}"
         )
     elif sts == "disconnected":
         smf.printf(
-            f"[!]{CC.YELLOW} ({sts}) Failed to connect. database offline.{CC.RESET}"
+            f"[!]{CC.YELLOW} ({sts}) => {rsn}.{CC.RESET}"
         )
     else:
-        smf.printf(f"[!]{CC.RED} ({sts}) Failed to connect to database.{CC.RESET}")
+        smf.printf(f"[!]{CC.RED} ({sts}) => Failed to connect to database.{CC.RESET}")
