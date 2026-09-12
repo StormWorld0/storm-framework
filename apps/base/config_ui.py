8e8b1c8d6a348119fe9e245dab1362d9b5e31220
エルジー🐧 <elzyproot@protonmail.com>
2026-09-12 20:10:12 +0700

    Update command guide in config_ui.py

--- apps/base/config_ui.py
+++ apps/base/config_ui.py
@@ -42,50 +42,72 @@ def show_help():
 {CC.MAGENTA}==========================================================================
 {CC.GREEN}                             COMMAND GUIDE
 {CC.MAGENTA}==========================================================================
+{CC.CYAN}
+CORE
+{CC.MAGENTA}----
 {CC.YELLOW}
-  show options                  : View the variables that have been set
-  show modules                  : Displaying module categories
-  show <categories>             : Displays the complete contents
-  show plugin                   : Displays existing plugins & plugin status
-  show wordlist                 : Displays a list of available wordlists
-  info <module_name>            : Complete Modules information
-  search <module_name>          : To search for modules, you can also use
-                                  filters such as (act:...) / (defact:...) /
-                                  (severity:...) / (cve:...) / (author:...)
+  use                           : To use the module, you can use <module_name> or <path_module>
+  set <var> <val>               : Filling values in variables
+  unset <var>                   : Delete value in a specific variable
+  unset all                     : Delete values in all variables
+  run                           : Run the selected module
 
+{CC.CYAN}
+DATABASE
+{CC.MAGENTA}--------
+{CC.YELLOW}
   db_status                     : View PostgreSQL database connection status
   db_connect                    : Connect to an external PostgreSQL database manually.
                                   Use the argument format: uname:pass@host:port/db
   db_disconnect                 : Serves to cleanly disconnect the database connection
-  
+
+{CC.CYAN}
+WORKSPACE
+{CC.MAGENTA}---------
   workspace                     : Displays currently available workspaces and hosts
   workspace add <ws_name>       : Adding a new workspace
   workspace del <ws_name>       : Deleting a specific workspace
   workspace <ws_name>           : Move to another workspace (if any)
-  
+
+{CC.CYAN}
+DATA
+{CC.MAGENTA}----
   services                      : Displays service data stored in the Postgres database
   vulns                         : Displays vulnerability data stored in the Postgres database
   hosts                         : Displays host data stored in the Postgres database
   creds                         : Displays credentials stored in the Postgres database
 
+{CC.CYAN}
+PLUGIN
+{CC.MAGENTA}------
+  load <plugin_name>            : Loading plugins into memory
+  unload <plugin_name>          : Remove plugins from memory
+  show plugin                   : Displays existing plugins & plugin status
+
+{CC.CYAN}
+UTILITY
+{CC.MAGENTA}-------
+  show options                  : View the variables that have been set
+  show modules                  : Displaying module categories
+  show wordlist                 : Displays a list of available wordlists
+  show <categories>             : Displays the complete contents
+  log export <val>              : Export logs from internal database and save as txt
+  log dump                      : Dump logs to the terminal screen
+  info <module_name>            : Complete Modules information
+  search <module_name>          : To search for modules, you can also use
+                                  filters such as (act:...) / (defact:...) /
+                                  (severity:...) / (cve:...) / (author:...)
+
+{CC.CYAN}
+SYSTEM
+{CC.MAGENTA}------
+{CC.YELLOW}
   help                          : Displaying the manual
   about                         : Information Development
   back                          : Back from current position
   clear                         : Clear command line
   exit                          : Exit the application
   restart                       : To restart if you experience a bug or error
-  
-  log export <val>              : Export logs from internal database and save as txt
-  log dump                      : Dump logs to the terminal screen
-  load <plugin_name>            : Loading plugins into memory
-  unload <plugin_name>          : Remove plugins from memory
-  unset <var>                   : Delete value in a specific variable
-  unset all                     : Delete values in all variables
-
-  use                           : To use the module, you can use <module_name> or <path_module>
-  set <var> <val>               : Filling values in variables
-  run                           : Run the selected module
-
   storm update                  : Make updates if necessary
 {CC.RESET}
 {CC.GREEN}
