569dafc8bf82e812ffe4c1b4b0b9b2c0b506e4de
エルジー🐧 <elzyproot@protonmail.com>
2026-09-25 19:22:08 +0700

    Rename REQUIRED_OPTIONS key from 'IP' to 'HOST'

--- modules/auxiliary/test_sock_udp.py
+++ modules/auxiliary/test_sock_udp.py
@@ -12,7 +12,7 @@ Testing Socket
     "License": "SMF License",
     "Date": "2026-09-17",
 }
-REQUIRED_OPTIONS = {"IP": ""}
+REQUIRED_OPTIONS = {"HOST": ""}
 
 
 def build_dns_query(domain: str, txid: int = 0x1234) -> bytes:
