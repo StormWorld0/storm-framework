10a242a1edb9dbd912b73357ec8c76b4108b5799
エルジー🐧 <elzyproot@protonmail.com>
2026-09-26 10:24:14 +0700

    Rename Constants class to ConstantsMix

--- lib/roar/crs/protocol/network/constants.py
+++ lib/roar/crs/protocol/network/constants.py
@@ -1,24 +1,17 @@
-class AF:
+class ConstantsMix:
     """Address Family"""
-
     AF_INET = "2"
     AF_INET6 = "10"
     AF_UNIX = "1"
     AF_UNSPEC = "0"
-
-
-class Type:
+    
     """Sock Type"""
-
     SOCK_STREAM = "1"
     SOCK_DGRAM = "2"
     SOCK_RAW = "3"
     SOCK_SEQPACKET = "5"
 
-
-class Proto:
     """Protocol"""
-
     IPPROTO_IP = "0"
     IPPROTO_ICMP = "1"
     IPPROTO_TCP = "6"
