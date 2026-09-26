0360a1509cd66e4b934c6bf101167a319f0ff972
エルジー🐧 <elzyproot@protonmail.com>
2026-09-26 09:52:51 +0700

    Rename import from 'socket' to 'Socket'

--- lib/roar/crs/protocol/__init__.py
+++ lib/roar/crs/protocol/__init__.py
@@ -1,5 +1,5 @@
 from .dns import requests  # noqa
 from .http import http_requests  # noqa
-from .network import socket  # noqa
+from .network import Socket  # noqa
 from .telnet import TelnetClient  # noqa
 from .whois import ipwhois, domwhois  # noqa
